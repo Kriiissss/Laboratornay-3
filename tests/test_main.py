bd5d537b8b1b8f43aac390b2b55f2f60bda4df1e
Kriiissss <klebedeva14052003@gmail.com>
2026-04-14 23:08:31 +0400

    style: fix pep8 formatting with black

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ def test_sum():
     # Простой тест на арифметику
     assert 1 + 1 == 2
 
+
 def test_string_lowercase():
     # Проверка, что метод работает правильно
     assert "HELLO".lower() == "hello"
