cba2aabc889088e0211a904f8a08d55f19b18859
Kriiissss <klebedeva14052003@gmail.com>
2026-04-14 23:01:23 +0400

    Fix: add tests and fix naming

---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+def test_sum():
+    # Простой тест на арифметику
+    assert 1 + 1 == 2
+
+def test_string_lowercase():
+    # Проверка, что метод работает правильно
+    assert "HELLO".lower() == "hello"
